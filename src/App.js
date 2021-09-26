c31fcbdefcbf1f453c2cb323e496b4cdd0864ad1
johnpersie <83553199+johnpersie@users.noreply.github.com>
2021-09-26 02:26:55 +0100

    Creating Navbar section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import react, { Component }from 'react';
+import react, { Component } from 'react';
+import Navbar from './components/layout/layout/Navbar';
 import './App.css';
 
 class  App extends Component {
 
   render() {
-
+    
     return (
-      <div className="App">
-        <h1>Hello From React</h1>
+      <div className='App'>
+      <Navbar />
       </div>
-    );
+    )
   }
 }
 
