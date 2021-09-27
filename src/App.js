09537e6a7a5c5e073cb8d736cba0b4bcea7722ad
johnpersie <83553199+johnpersie@users.noreply.github.com>
2021-09-27 23:39:24 +0100

    building the users profile section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import react, { Component } from 'react';
+import React, { Component } from 'react';
 import Navbar from "./components/layout/Navbar";
-import UserItem from "./components/users/Useritem";
+import Users from './components/users/Users';
 import './App.css';
 
 class  App extends Component {
@@ -10,7 +10,7 @@ class  App extends Component {
     return (
       <div className='App'>
       <Navbar /*title=" Github Finder" icon="fab fa-github"*//>
-      <UserItem />
+      <Users />
       </div>
     )
   }
--- src/components/users/Useritem.js
+++ src/components/users/Useritem.js
@@ -1,22 +1,19 @@
 import React, { Component } from 'react'
 
 class UserItem extends Component {
-    state = {
-        id: "id",
-        login: "mojombo",
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: 'https://github.com/mojombo'
-    };
 
     render() {
+        const {login, avatar_url, html_url} = this.props.user;
+
         return (
             <div className="card text-center">
-                <img src={this.state.avatar_url} 
-                alt="" className="round-img" 
+                <img src={avatar_url} 
+                alt=''
+                className="round-img" 
                 style={{width: "60px"}} />
                 <div>
-                    <h3>{this.state.login}</h3>
-                    <a href={this.state.html_url} className="btn btn-dark btn-sm my-1">
+                    <h3>{login}</h3>
+                    <a href={html_url} className="btn btn-dark btn-sm my-1">
                         More
                     </a>
                 </div>
