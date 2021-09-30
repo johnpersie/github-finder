70ec46d68691ed10c5e8e12ffd07cd887894e0d8
johnpersie <83553199+johnpersie@users.noreply.github.com>
2021-09-30 17:14:42 +0100

    converting class base component to functional component

--- src/components/users/Useritem.js
+++ src/components/users/Useritem.js
@@ -1,24 +1,29 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const UserItem = (props) => {
+const UserItem = ({user: { login, avatar_url, html_url } }) => {
 
-        const {login, avatar_url, html_url} = props.user;
-
-        return (
-            <div className="card text-center">
-                <img src={avatar_url} 
-                alt=''
-                className="round-img" 
-                style={{width: "60px"}} />
-                <div>
-                    <h3>{login}</h3>
-                    <a href={html_url} className="btn btn-dark btn-sm my-1">
-                        More
-                    </a>
-                </div>
+    return (
+        <div className="card text-center">
+            <img 
+            src={avatar_url} 
+            alt=''
+            className="round-img" 
+            style={{width: "60px"}} 
+            />
+            
+            <h3>{login}</h3>
+            <div>
+                <a href={html_url} className="btn btn-dark btn-sm my-1">
+                    More
+                </a>
             </div>
-        )
+        </div>
+    )
 }
 
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired
+}
 export default UserItem
 
